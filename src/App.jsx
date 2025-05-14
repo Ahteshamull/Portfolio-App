43aaf76bc7ca6eb80b83e751c0a9b97e23da53c5
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-05-14 14:32:04 +0600

    font famelly and strecmode add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
+import Work from './components/Work/Work'
+import Skill from './components/Skills/Skill'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import Education from './components/Education/Education'
+import Experience from './components/Experience/Experience'
 
 export default function App() {
   return (
-    <div className='bg-slate-400'>
-      ddfs
+    <div className="bg-[#050414]">
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      <div className='relative pt-20'>
+        <Navbar />
+        <About />
+        <Experience />
+        <Skill />
+        <Work />
+        <Education />
+        <Contact />
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
